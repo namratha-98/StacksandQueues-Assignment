e59b9e42ccc05627372a2a56355fb3afe1512332
namratha-98 <74010135+namratha-98@users.noreply.github.com>
2020-12-29 21:56:11 +0530

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,37 @@
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
+#include<stdint.h>
+#define MAX_DEPTH 32
+#include<stddef.h>
+#include<stdlib.h>
+#define STACK_OK 1
+#define STACK_FULL 2
+#define STACK_EMPTY 3
+
+typedef struct _stack_ Stack;
+
+struct _stack_
+{
+	char data[MAX_DEPTH];
+	int32_t top;
+	int32_t size;
+
+};
+
+typedef struct _stackResult_ Stack_Result;
+struct _stackResult_
+{
+	char data;
+	int32_t status;
+};
+
+Stack stack_new(int32_t size);
+Stack* stack_push(Stack *stk,char data,Stack_Result * res);
+Stack* stack_pop(Stack *stk,Stack_Result *res);
+Stack* stack_peek(Stack *stk,Stack_Result *res);
+
+#endif
+
+
+
+#endif // STACK_H_INCLUDED
